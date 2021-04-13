5eee785f25aaa0d72ddb74ce82e96737655ffafb
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-13 07:18:35 +0100

    Created layout components for the Sidebar index page.

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { FaTimes } from 'react-icons/fa'
 
-export const SidebarContainer = styled.asided`
+export const SidebarContainer = styled.aside`
     position: fixed;
     z-index: 999;
     width: 100%;
@@ -12,8 +12,9 @@ export const SidebarContainer = styled.asided`
     top: 0;
     left: 0;
     transition: 0.3s ease-in-out;
-    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-    top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
+    /* opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+    top: ${({ isOpen }) => (isOpen ? '0' : '-100%')}; */
+    top: 0;
 `;
 
 export const CloseIcon = styled(FaTimes)`
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,6 +7,25 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover">
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="services">
+                        Services
+                    </SidebarLink>
+                    <SidebarLink to="signup">
+                        Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
         </SidebarContainer>
     )
 }
