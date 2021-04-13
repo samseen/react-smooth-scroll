8a7a503047f0d2663d591ae1f0114551e442a0f5
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-13 07:35:42 +0100

    Displayed the mobile menu dropdown.

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -37,11 +37,22 @@ export const SidebarWrapper = styled.div`
     color: #fff;
 `;
 
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(6, 60px);
+    }
+`
+
 export const SidebarLink = styled(LinkS)`
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 5rem;
+    font-size: 1.5rem;
     text-decoration: none;
     list-style: none;
     transition: 0.2s ease-in-out;
