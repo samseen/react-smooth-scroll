9664d48dc94cf7262215c932267b3ba4037c920c
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-17 06:27:57 +0100

    Added hero section components for the text on the video.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -8,6 +8,15 @@ const HeroSection = () => {
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <HeroH1>Virtual Banking Made Easy</HeroH1>
+                <HeroP>Sign Up for a new account today and receive $250 in credit towards your next payment.</HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup">
+                        Get Started { hover ? <ArrowForward /> : <ArrowRight /> }
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
