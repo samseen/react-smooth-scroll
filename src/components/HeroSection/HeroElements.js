3237bce04c43d71a18701df31ae5ed58d08d06c9
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-17 06:42:48 +0100

    Added HeroP and HeroH1 to the styled components.

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -31,3 +31,43 @@ export const VideoBg = styled.video`
     object-fit: cover;
     background: #232a34;
 `;
+
+export const HeroContent = styled.div`
+    z-index: 3;
+    max-width: 1200px;
+    position: absolute;
+    padding: 8px 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const HeroH1 = styled.h1`
+    color: #fff;
+    font-size: 48px;
+    text-align: center;
+
+    @media screen and (max-width: 768px) {
+        font-size: 40px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 32px;
+    }
+`;
+
+export const HeroP = styled.p`
+    margin-top: 24px;
+    color: #fff;
+    font-size: 24px;
+    text-align: center;
+    max-width: 600px;
+
+    @media screen and (max-width: 768px) {
+        font-size: 240px;
+    }
+
+    @media screen and (max-width: 480px) {
+        font-size: 18px;
+    }
+`;
