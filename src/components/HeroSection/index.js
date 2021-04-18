c1a94f30776033c4672710c5767159a21058a5a9
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-18 22:00:33 +0100

    Made the main hero button darker with attributes.

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Video from '../../videos/video.mp4';
+import {Button} from '../ButtonElement';
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements';
 
 const HeroSection = () => {
@@ -18,7 +19,8 @@ const HeroSection = () => {
                 <HeroH1>Virtual Banking Made Easy</HeroH1>
                 <HeroP>Sign Up for a new account today and receive $250 in credit towards your next payment.</HeroP>
                 <HeroBtnWrapper>
-                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover} primary="true"
+                    dark='true'>
                         Get Started { hover ? <ArrowForward /> : <ArrowRight /> }
                     </Button>
                 </HeroBtnWrapper>
