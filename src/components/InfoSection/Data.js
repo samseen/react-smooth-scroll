5def27ec86aa479e78640b1c6ac11a67f4f466d1
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-21 09:26:54 +0100

    Added SVG images and populated the data file.

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fee.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: requestAnimationFrame('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
