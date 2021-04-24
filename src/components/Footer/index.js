0c4c5f22a2ab3bb9402bd3358443b92deb6ec116
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-24 06:17:55 +0100

    Changed the footer main texts.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,33 +16,31 @@ const Footer = () => {
                             <FooterLink to="/signin">Terms of Service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to="/">Submit Video</FooterLink>
+                            <FooterLink to="/">Ambassadors</FooterLink>
+                            <FooterLink to="/">Agency</FooterLink>
+                            <FooterLink to="/">Influencer</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to="/">Contact</FooterLink>
+                            <FooterLink to="/">Support</FooterLink>
+                            <FooterLink to="/">Destinations</FooterLink>
+                            <FooterLink to="/">Sponsorships</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to="/signin">How it works</FooterLink>
-                            <FooterLink to="/signin">Testimonials</FooterLink>
-                            <FooterLink to="/signin">Careers</FooterLink>
-                            <FooterLink to="/signin">Investors</FooterLink>
-                            <FooterLink to="/signin">Terms of Service</FooterLink>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to="/">Instagram</FooterLink>
+                            <FooterLink to="/">Facebook</FooterLink>
+                            <FooterLink to="/">Youtube</FooterLink>
+                            <FooterLink to="/">Twitter</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                
             </FooterWrap>
         </FooterContainer>
     )
