a59f034473a1726abc93432e15057264d5c1d5ae
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-26 23:14:48 +0100

    Exported the remaining Form Element Items.

--- src/components/Signin/SigninElements.js
+++ src/components/Signin/SigninElements.js
@@ -65,3 +65,41 @@ export const Form = styled.form`
         padding: 32px 32px;
     }
 `
+
+export const FormH1 = styled.h1`
+    margin-bottom: 40px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 400;
+    text-align: center;
+`
+
+export const FormLabel = styled.label`
+    margin-bottom: 8px;
+    font-size: 14px;
+    color: #fff;
+`
+
+export const FormInput = styled.input`
+    padding: 16px 16px;
+    margin-bottom: 32px;
+    border: none;
+    border-radius: 4px;
+`
+
+export const FormButton = styled.button`
+    background: #01bf71;
+    padding: 16px 0;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+`
+
+export const Text = styled.span`
+    text-align: center;
+    margin-top: 24px;
+    color: #fff;
+    font-size: 14px;
+`
